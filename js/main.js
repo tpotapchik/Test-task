71366f04f49409dc763e57212ccf612d242f62e8
tpotapchik <tanyapotapchik@gmail.com>
2015-02-04 16:57:50 +0300

    header

--- js/main.js
+++ js/main.js
@@ -9,114 +9,116 @@ $(function () {
                 }
             });
 
-    $('.slider').slick({
-        dots: false
+    $('.gallery').slick({
+        dots: true,
+       // autoplay: true,
+        autoplaySpeed: 2000
     });
-    $('.partners').slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 300
-    });
-
-    $('.medium-select').chosen({
-        disable_search: true,
-        inherit_select_classes: true,
-        width: "200px"
-    });
-    $('.small-select').chosen({
-        disable_search: true,
-        inherit_select_classes: true,
-        width: "100px"
-    });
-
-    $('.more-btn').click(function () {
-        var $this = $(this);
-        $this.closest('.main-text-block-wrap').find('.news-body').slideToggle();
-
-        if ($this.hasClass('opened')) {
-            $this.text('Читать полностью');
-        } else {
-            $this.text('Свернуть');
-        }
-        $this.toggleClass('opened');
-
-    });
-
-    $(".fancybox").fancybox({
-        nextEffect:'fade',
-        prevEffect:'fade'
-    });
-
-    $(window).on('scroll', function () {
-        if ($(document).scrollTop() >= 900) {
-            $('.back-top').fadeIn();
-        }
-        else {
-            $('.back-top').fadeOut();
-        }
-    });
-
-    $('.back-top').click(function () {
-        $('body,html').animate({
-            'scrollTop': 0
-        }, 400)
-    });
-
-    $(".popup").fancybox();
-
-    $('#js-submit').click(function (e) {
-        e.preventDefault();
-        var $this = $(this);
-        var popup = $('.order-call');
-        var message = popup.find('.message');
-        var $form = popup.find('form');
-        var $inputName = $form.find('input[type="text"]');
-        var $inputTel = $form.find('input[type="tel"]');
-
-        if ($inputName.val() == '') {
-            $inputName.addClass('error');
-
-        } else if ($inputTel.val() == '') {
-            $inputTel.addClass('error');
-
-        } else if (!validatePhone($inputTel.val())) {
-            $inputTel.addClass('error');
-
-        } else {
-            $this.attr('disabled', 'disabled');
-            $.ajax({
-                url: $form.attr('action'),
-                type: "POST",
-                data: $form.serialize(),
-                success: function (data) {
-
-                    if (data.error) {
-                        alert('Произошла ошибка');
-                    } else {
-                        message.slideDown();
-
-                        setTimeout(function () {
-                            $.fancybox.close();
-                            $form[0].reset();
-                            message.hide();
-                            $this.removeAttr('disabled');
-                        }, 5000);
-                    }
-                },
-                error: function () {
-                    alert('Произошла ошибка. Попробуйте еще раз.');
-                }
-            });
-        }
-    });
-
-    $('.order-call form').find('input').focus(function () {
-        $(this).removeClass('error');
-    })
+    //$('.partners').slick({
+    //    dots: false,
+    //    infinite: true,
+    //    slidesToShow: 6,
+    //    slidesToScroll: 1,
+    //    autoplay: true,
+    //    autoplaySpeed: 2000,
+    //    speed: 300
+    //});
+    //
+    //$('.medium-select').chosen({
+    //    disable_search: true,
+    //    inherit_select_classes: true,
+    //    width: "200px"
+    //});
+    //$('.small-select').chosen({
+    //    disable_search: true,
+    //    inherit_select_classes: true,
+    //    width: "100px"
+    //});
+    //
+    //$('.more-btn').click(function () {
+    //    var $this = $(this);
+    //    $this.closest('.main-text-block-wrap').find('.news-body').slideToggle();
+    //
+    //    if ($this.hasClass('opened')) {
+    //        $this.text('Читать полностью');
+    //    } else {
+    //        $this.text('Свернуть');
+    //    }
+    //    $this.toggleClass('opened');
+    //
+    //});
+    //
+    //$(".fancybox").fancybox({
+    //    nextEffect:'fade',
+    //    prevEffect:'fade'
+    //});
+    //
+    //$(window).on('scroll', function () {
+    //    if ($(document).scrollTop() >= 900) {
+    //        $('.back-top').fadeIn();
+    //    }
+    //    else {
+    //        $('.back-top').fadeOut();
+    //    }
+    //});
+    //
+    //$('.back-top').click(function () {
+    //    $('body,html').animate({
+    //        'scrollTop': 0
+    //    }, 400)
+    //});
+    //
+    //$(".popup").fancybox();
+    //
+    //$('#js-submit').click(function (e) {
+    //    e.preventDefault();
+    //    var $this = $(this);
+    //    var popup = $('.order-call');
+    //    var message = popup.find('.message');
+    //    var $form = popup.find('form');
+    //    var $inputName = $form.find('input[type="text"]');
+    //    var $inputTel = $form.find('input[type="tel"]');
+    //
+    //    if ($inputName.val() == '') {
+    //        $inputName.addClass('error');
+    //
+    //    } else if ($inputTel.val() == '') {
+    //        $inputTel.addClass('error');
+    //
+    //    } else if (!validatePhone($inputTel.val())) {
+    //        $inputTel.addClass('error');
+    //
+    //    } else {
+    //        $this.attr('disabled', 'disabled');
+    //        $.ajax({
+    //            url: $form.attr('action'),
+    //            type: "POST",
+    //            data: $form.serialize(),
+    //            success: function (data) {
+    //
+    //                if (data.error) {
+    //                    alert('Произошла ошибка');
+    //                } else {
+    //                    message.slideDown();
+    //
+    //                    setTimeout(function () {
+    //                        $.fancybox.close();
+    //                        $form[0].reset();
+    //                        message.hide();
+    //                        $this.removeAttr('disabled');
+    //                    }, 5000);
+    //                }
+    //            },
+    //            error: function () {
+    //                alert('Произошла ошибка. Попробуйте еще раз.');
+    //            }
+    //        });
+    //    }
+    //});
+    //
+    //$('.order-call form').find('input').focus(function () {
+    //    $(this).removeClass('error');
+    //})
 
 
 });
